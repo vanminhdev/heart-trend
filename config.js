539142e3fbe9af57814ea45b4c1b38227e567980
vanminh <vanminh.dev@gmail.com>
2022-11-12 00:15:01 +0700

    init

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+    lines: [
+        "Anh Yêu Em",
+        "Linh à",
+        "Hihi",
+        "tim nè ❤ ❤ ❤ :3",
+    ],
+    date: "2022-03-25T20:00:00",
+    names: ["Minh", "Linh"]
+}
